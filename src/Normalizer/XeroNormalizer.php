bca77de541de128b172ce5db2bf698143e8222dc
Matthew Radcliffe <mradcliffe@softpixel.com>
2015-04-25 18:52:50 -0400

    Add support for normalization with test.

--- src/Normalizer/XeroNormalizer.php
+++ src/Normalizer/XeroNormalizer.php
@@ -26,6 +26,37 @@ class XeroNormalizer extends ComplexDataNormalizer implements DenormalizerInterf
   /**
    * {@inheritdoc}
    */
+  public function normalize($object, $format = NULL, array $context = array()) {
+    // Get the array map.
+    $data = array();
+    $items = array();
+    $ret = array();
+
+    foreach ($object as $n => $item) {
+      if (!isset($plural_name)) {
+        $plural_name = $item::$plural_name;
+        $name = $item::$xero_name;
+      }
+
+      $data = parent::normalize($item, $format, $context);
+      $data = $this->reduceEmpty($data);
+      $items[] = $data;
+    }
+    $ret[$plural_name] = [$name => NULL];
+
+    if (count($items) === 1) {
+      $ret[$plural_name][$name] = $items[0];
+    }
+    else {
+      $ret[$plural_name][$name] = $items;
+    }
+
+    return $ret;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function denormalize($data, $class, $format = NULL, array $context = array()) {
     // The context array requires the Xero data type to be known. If not, then
     // cannot do anything. This is consistent with Entity normalization.
@@ -45,6 +76,33 @@ class XeroNormalizer extends ComplexDataNormalizer implements DenormalizerInterf
     $items = $this->typedDataManager->create($list_definition, $data[$plural_name][$name]);
 
     return $items;
+  }
+
+  /**
+   * Remove null values from normalized items.
+   *
+   * @param $value
+   *   The value to reduce.
+   * @return mixed
+   *   Either FALSE or the value.
+   */
+  protected function reduceEmpty($value) {
+    if (is_array($value)) {
+      foreach ($value as $n => $item) {
+        $item = $this->reduceEmpty($item);
+        if ($item) {
+          $value[$n] = $item;
+        }
+        else {
+          unset($value[$n]);
+        }
+      }
+    }
+    else if (empty($value)) {
+      return FALSE;
+    }
+
+    return $value;
   }
 
 }
