0eae5e1ecdeee8d2463de53d4a6acae6416b963e
Matthew Radcliffe <mradcliffe@softpixel.com>
2015-08-07 10:27:14 -0400

    Use git instead of drush because drush

--- scripts/composer-manager.sh
+++ scripts/composer-manager.sh
@@ -5,7 +5,7 @@ set -e $DRUPAL_TI_DEBUG
 drupal_ti_ensure_drupal
 
 cd "$DRUPAL_TI_DRUPAL_DIR"
-drush dl composer_manager --yes
+git clone --depth 1 --branch 8.x-1.x https://git.drupal.org/project/composer_manager.git modules/composer_manager
 chmod +x modules/composer_manager/scripts/init.sh
 ./modules/composer_manager/scripts/init.sh
 cd "$DRUPAL_TI_DRUPAL_DIR/core"
